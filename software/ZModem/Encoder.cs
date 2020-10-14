d58c2b1287e24afc3590f16e44953c080d08e4a7
qitas <qitas@qitas.cn>
2020-10-14 17:48:05 +0800

    添加文件

---
+++ software/ZModem/Encoder.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ZModem.Constants;
+
+namespace ZModem
+{
+    public static class ZDLEEncoder
+    {
+        /// <summary>
+        /// Zmodem software escapes ZDLE (0x18), 0x10, 0x90, 0x11, 0x91, 0x13, and 0x93. 
+        /// </summary>
+        private static readonly byte[] EscapeCharacters = new byte[] {
+            (byte)ControlBytes.ZDLE,
+            (byte)ControlBytes.XON,
+            (byte)ControlBytes.XOFF,
+            (byte)ControlBytes.XON | 0x80,
+            (byte)ControlBytes.XOFF | 0x80,
+            (byte)ControlBytes.DLE,
+            (byte)ControlBytes.CTRL0x90,
+            };
+
+        /// <summary>
+        /// If preceded by 0x40 or 0xc0 (@), 0x0d and 0x8d are also escaped to
+        /// protect the Telenet command escape CR-@-CR. The receiver ignores
+        /// 0x11, 0x91, 0x13, and 0x93 characters in the data stream. 
+        /// </summary>
+        private static readonly byte[] SpeicalEscapeCharacterTrigger = new byte[] {
+            (byte)ControlBytes.ATSymbol,
+            (byte)ControlBytes.À
+        };
+        private static readonly byte[] SpecialEscapeCharacters = new byte[] {
+            (byte)ControlBytes.CR,
+            (byte)ControlBytes.RI
+        };
+
+
+        public static byte[] EscapeControlCharacters(byte[] src)
+        {
+            var result = new List<byte>();
+
+            for (int i = 0; i < src.Length; i++)
+            {
+                var b = src[i];
+
+                if (b == (byte)ControlBytes.ZDLE)
+                {
+                    var escape = new byte[] { (byte)ControlBytes.ZDLE, (byte)ControlBytes.ZDLEE };
+                    result.AddRange(escape);
+                }
+                else if (EscapeCharacters.Contains(b))
+                {
+                    var c = (char)(b | 0x40);
+
+                    var escape = new byte[] { (byte)ControlBytes.ZDLE, (byte)c };
+                    result.AddRange(escape);
+                }
+                else if (SpecialEscapeCharacters.Contains(b))
+                {
+                    var c = (char)(b | 0x40);
+                    var escape = new byte[] { (byte)ControlBytes.ZDLE, (byte)c };
+                    result.AddRange(escape);
+                }
+                else
+                {
+                    result.Add(b);
+                }
+            }
+
+            return result.ToArray();
+        }
+    }
+}
---
+++ software/ZModem/ResponseHeader.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Linq;
+using ZModem.Constants;
+
+namespace ZModem
+{
+    public class ResponseHeader
+    {
+        public ResponseHeader(byte[] buffer)
+        {
+            try
+            {
+                // Expected buffer length is 21.
+                // In some cases the buffer length is longer than expected.
+                // So we slice the buffer to get the response we want.
+                if (buffer.Length > 21)
+                {
+                    var offset = buffer.Length - 21;
+                    buffer = buffer
+                        .Skip(offset)
+                        .Take(21)
+                        .ToArray();
+                }
+
+
+                if (buffer.Length < 20 || buffer.Length > 21)
+                {
+                    Console.WriteLine(@"Wrong header ¯\_(ツ)_/¯");
+                    return;
+                }
+
+                // Cast to chars
+                var chars = buffer.Select(x => (char)x).ToArray();
+
+                // Set binary header
+                BinaryHeader = chars.Take(3).ToArray();
+                BinaryHeaderHuman = string.Join("", BinaryHeader);
+                FullHeaderHuman = string.Join("", chars);
+
+                // Set frame indicator
+                FrameIndicator = (ControlBytes)chars[3];
+
+                // Set header
+                var headerInt = Utils.GetIntFromHex(chars[4], chars[5]);
+                Enum.TryParse(headerInt.ToString(), out HeaderType header);
+                ZHeader = header;
+
+                // Pars arguments
+                ZP0 = Utils.GetIntFromHex(chars[6], chars[7]);
+                ZP1 = Utils.GetIntFromHex(chars[8], chars[9]);
+                ZP2 = Utils.GetIntFromHex(chars[10], chars[11]);
+                ZP3 = Utils.GetIntFromHex(chars[12], chars[13]);
+
+                if (ZHeader == HeaderType.ZRPOS || ZHeader == HeaderType.ZACK)
+                {
+                    RequestedOffset = Utils.GetIntFromZModemOffset(ZP0, ZP1, ZP2, ZP3);
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        public char[] BinaryHeader { get; private set; }
+
+        public string BinaryHeaderHuman { get; private set; }
+
+        public string FullHeaderHuman { get; set; }
+
+        public ControlBytes FrameIndicator { get; private set; }
+
+        public HeaderType ZHeader { get; private set; } = HeaderType.None;
+
+        public int ZP0 { get; private set; }
+
+        public int ZP1 { get; private set; }
+
+        public int ZP2 { get; private set; }
+
+        public int ZP3 { get; private set; }
+
+        public int RequestedOffset { get; set; }
+    }
+}
